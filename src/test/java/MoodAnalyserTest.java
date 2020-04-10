08ac87fb423f39c9d97cd677c503c9bdc3ac2811
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-10 17:21:07 +0530

    Added:Test case for returning happy message

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -2,7 +2,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-
     MoodAnalyser moodAnalyser = new MoodAnalyser();
     String result;
     // Test case for Given “I am in Sad Mood” message should return SAD
@@ -10,5 +9,12 @@ public class MoodAnalyserTest {
     public void givenMessage_WhenProper_RespondAsSadMood() {
         result = moodAnalyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals("SAD", result);
+    }
+    // Test case for Given “I am in Any Mood” message should return Happy
+    @Test
+    public void givenMessage_WhenProper_RespondAsHappyMood()
+    {
+        result = moodAnalyser.analyseMood("I am in Any Mood");
+        Assert.assertEquals("HAPPY",result);
     }
 }
